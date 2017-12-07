ac82b0d30e0786e95b2fa9e0510c48f3f46c0ec0
Dominik Hassler <hadfl@omniosce.org>
2017-12-07 18:48:46 +0100

    add line to PkgUpd output

--- lib/OOCEapps/Controller/PkgUpd.pm
+++ lib/OOCEapps/Controller/PkgUpd.pm
@@ -48,6 +48,8 @@ my $getPkgAvailVer = sub {
                     $pkgList->{$pkg}->{notes} ]
                     if versioncmp($pkgList->{$pkg}->{version}, $latest); 
             }
+            push @data, '---';
+
             $self->render(json => OOCEapps::Mattermost->table(\@data));
         }
     );
