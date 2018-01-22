156745543caaa59377bc94574e39a9370dbffec0
Dominik Hassler <hadfl@omniosce.org>
2018-01-22 14:43:31 +0100

    increase 'inactivity_timeout' for PkgUpd

--- lib/OOCEapps/Controller/base.pm
+++ lib/OOCEapps/Controller/base.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Mojolicious::Controller';
 use OOCEapps::Mattermost;
 
 # attributes
-has ua      => sub { shift->app->ua };
+has ua      => sub { shift->app->ua->new };
 has module  => sub { ref shift };
 has name    => sub { lc ((split /::/, shift->module)[-1]) };
 has config  => sub { my $self = shift; $self->app->config->{MODULES}->{$self->name} };
@@ -27,7 +27,7 @@ __END__
 
 =head1 COPYRIGHT
 
-Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
+Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 
 =head1 LICENSE
 
