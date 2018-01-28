768be4a2452b438339f4bd659d6c910d6d09974f
Tobias Oetiker <tobi@oetiker.ch>
2018-01-28 19:05:17 +0100

    added crypto code

--- lib/OOCEapps/Utils.pm
+++ lib/OOCEapps/Utils.pm
@@ -1,5 +1,8 @@
 package OOCEapps::Utils;
 use Mojo::Base -base;
+use Mojo::JSON qw(encode_json decode_json);
+use Mojo::Util qw(b64_encode b64_decode encode decode);
+use Crypt::Ed25519;
 
 use File::Spec qw(catdir splitpath);
 
@@ -79,6 +82,85 @@ sub elemOf {
     }
 }
 
+=head2 pack($hash,$secret_key)
+
+pack the content of the hash, adding a timestamp and a signature in the process. Returns a b64 encoded string.
+The secret_key is generated with C<Crypt::Ed25519::eddsa_secret_key>.
+
+=cut
+
+my $dataWalker;
+
+sub pack {
+    my $self = shift;
+    my $data = shift;
+    my $secret_key = shift;
+    $data = { %$data };
+    # add a timestamp guard against replay attacks
+    $data->{__timestamp__} = time;
+
+    # add a nonce to to make sure the signature
+    # is different every time around
+    $data->{__nonce__} = rand;
+
+    # make sure we end up with a regular text string
+    # to build the signature on and not something encoded
+    my $content = encode('UTF-8',$dataWalker->($data));
+
+    $data->{__signature__} = b64_encode(Crypt::Ed25519::eddsa_sign($content,Crypt::Ed25519::eddsa_public_key $secret_key,$secret_key),'');
+    return b64_encode(encode_json($data), "");
+}
+
+=head2 unpack($b64string,$public_key[,$pack_validity])
+
+unpack the given string, checking the timestamp (for maximum age), the signature vor validity and the presence of a nonce.
+if anything fails, the method dies.
+
+The public_key is generated with C<Crypt::Ed25519::eddsa_public_key $secret_key>.
+
+How old can a data pack be to be considered valid.
+
+=cut
+
+sub unpack {
+    my $self = shift;
+    my $json = b64_decode(shift);
+    my $data = decode_json($json);
+    my $public_key = shift;
+    my $signature = b64_decode(delete $data->{__signature__}) // die "invalid packege 2";
+
+    # first remove the signature then walk the remaining data
+    # to get the basis to check the signature
+    my $content = encode('UTF-8',$dataWalker->($data));
+
+    # finally remove the other bits that were added
+    # in the pack routine above
+    my $nonce = delete $data->{__nonce__} // die "invalid package 3";
+    my $timestamp = delete $data->{__timestamp__} // die "invalid package 1";
+    my $now = time;
+    if (Crypt::Ed25519::eddsa_verify $content, $public_key, $signature){
+        if (not $packValidaity or $now - $timestamp < $packValidity ){
+            return $data;
+        }
+        die "invalid package 5 now:$now - ts:$timestamp >= ".$packValidity;
+    }
+    die "invalid package 4";
+}
+
+
+$dataWalker = sub {
+    my $data = shift;
+    my $antiLoop = shift // {};
+    for (ref $data){
+        next if $antiLoop->{''.$data};
+        $antiLoop->{''.$data} = 1;
+        /ARRAY/ && return join "\n", map { $dataWalker->($_, $antiLoop) } @$data;
+        /HASH/ &&  return join "\n", map { $dataWalker->($data->{$_}, $antiLoop) } sort keys %$data;
+        /^$/ && return $data;
+        die "Unknown data type '$_'";
+    }
+};
+
 1;
 
 __END__
@@ -103,10 +185,11 @@ this program. If not, see L<http://www.gnu.org/licenses/>.
 =head1 AUTHOR
 
 S<Dominik Hassler E<lt>hadfl@omniosce.orgE<gt>>
+S<Tobias Oetiker E<lt>tobi@omniosce.orgE<gt>>
 
 =head1 HISTORY
 
 2017-09-06 had Initial Version
-
+2018-01-28 to Added Crypt Tools
 =cut
 
